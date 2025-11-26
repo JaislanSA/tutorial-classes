b2b9ec87dfa9c714b9bdc8aa4a5858bc16b8dc21
JaislanSA <jaislanaraujo6@gmail.com>
2025-11-26 20:41:36 -0300

    first commit

---
+++ src/customer.ts
@@ -0,0 +1,16 @@
+export default class Customer {
+
+    nome: string;
+    age: number;
+    email: string;
+    birthDate: Date = new Date();
+
+    constructor(nome: string, email: string, birthDate: Date)
+
+    {
+        this.nome = nome;
+        this.email = email;
+        this.age = new Date().getFullYear() - birthDate.getFullYear();
+        this.birthDate = birthDate;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Customer from "./Customer";
+
+const customer1 = new Customer("Alice","contato@alice.com.br", new Date("1990-05-15"));
+
+
+console.log(customer1);
